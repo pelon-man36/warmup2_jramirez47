70d3d78031588cfb98f48584d3df393dc1f9d725
Johan Ramirez <johand200505@gmail.com>
2026-09-17 14:04:22 -0400

    Warmup 2 file created with Instructor Jonathon's slides. Also initialized gitignore file.

---
+++ warmup2_jramirez47.py
@@ -0,0 +1,51 @@
+from direct.showbase.ShowBase import ShowBase
+import sys, math, random
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.fighter = self.loader.loadModel("./Assets/sphere.egg")
+        self.fighter.reparentTo(self.render)
+        self.fighter.setColorScale(1.0, 0.0, 0.0, 1.0)
+
+        self.disableMouse()
+
+        self.camera.setPos(0.0, 0.0, 250.0)
+        self.camera.setHpr(0.0, -90.0, 0.0)
+
+        self.accept("escape", self.quit)
+        self.accept("arrow_left", self.negativeX, [1])
+        self.accept("arrow_left-up", self.negativeX, [0])
+
+        self.parent = self.loader.loadModel("./Assets/cube.egg")
+
+        x = 0 # The zero position of the circle. We will go by radians around the circle and create objects.
+        for i in range(100):
+            theta = x
+            self.placeholder2 = self.render.attachNewNode("PlaceHolder") # Attaching a new node to the render scene graph.
+            self.placeholder2.setPos(50.0 * math.cos(theta), 50.0 * math.sin(theta), 0.0 * math.tan(theta))
+            red = 0.6 + random.random() * 0.4
+            green = 0.6 + random.random() * 0.4
+            blue = 0.6 + random.random() * 0.4
+            self.placeholder2.setColorScale(red, green, blue, 1.0)
+            self.parent.instanceTo(self.placeholder2)
+            x = x + 0.06 # Adding to the radians spacing. The last object will be placed just 
+            # to the left of the starting one if we keep keep the increments of this loop to 100
+
+    def negativeX(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.moveNegativeX, "moveNegativeX")
+        else:
+            self.taskMgr.remove("moveNegativeX")
+
+    def moveNegativeX(self, task):
+        self.fighter.setX(self.fighter, - 1)
+        return task.cont # Sets the task to continue next game cycle.
+
+    def quit(self):
+        sys.exit()
+
+
+app = MyApp()
+app.run()
